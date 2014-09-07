4feb2ccbaca976ece13a8d17c76af9e45c603cc3
Xepoof <xep@matheus.ninja>
2014-09-07 18:42:47 -0300

    Hands. typing words. Things. being fixed.

--- src/main/java/com/mythicmc/mythic/utils/Logger.java
+++ src/main/java/com/mythicmc/mythic/utils/Logger.java
@@ -2,16 +2,19 @@ package com.mythicmc.mythic.utils;
 
 public class Logger {
 
-    public static void info(String msg) {
-        System.out.println("[Mythic][INFO] " + msg);
+    public static void info(String format, Object...objects) {
+        logf("info", format, objects);
     }
 
-    public static void warn(String msg) {
-        System.out.println("[Mythic][WARN] " + msg);
+    public static void warn(String format, Object...objects) {
+        logf("warn", format, objects);
     }
 
-    public static void error(String msg) {
-        System.out.println("[Mythic][ERROR] " + msg);
+    public static void error(String format, Object...objects) {
+        logf("error", format, objects);
     }
-
+    
+    public static void logf(String level, String format, Object...objects) {
+    	System.out.printf(String.format("[%s/%s]%s", "Mythic", level.toUpperCase(), format), objects);	
+    }
 }
