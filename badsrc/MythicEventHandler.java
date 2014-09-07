c863d9fa59cd528a3dcd0c9cda79357658bb16b5
AzureusNation <alphatango369@gmail.com>
2014-09-07 21:02:52 +0100

    Does this work?

--- badsrc/MythicEventHandler.java
+++
@@ -1,8 +0,0 @@
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface MythicEventHandler { }
