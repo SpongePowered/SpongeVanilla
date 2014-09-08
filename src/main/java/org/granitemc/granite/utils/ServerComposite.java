5703e30a5cf2fdcd40e58761b120922130d285c6
Jim C K Flaten <jckf@jckf.no>
2014-09-08 21:23:37 +0200

    Removed unused imports.

--- src/main/java/org/granitemc/granite/utils/ServerComposite.java
+++ src/main/java/org/granitemc/granite/utils/ServerComposite.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Field;
-import java.util.Objects;
 
 public class ServerComposite {
     static Class<?> serverClass = null;
