28d6cbd7b3f64826d689dfb3f78e180178961cb1
avarisc <avarisc@gmail.com>
2014-09-10 00:46:31 -0800

    Added some CommandSender utils and an example command

--- src/main/java/org/granitemc/granite/utils/CommandProxy.java
+++ src/main/java/org/granitemc/granite/utils/CommandProxy.java
@@ -2,22 +2,76 @@ package org.granitemc.granite.utils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandProxy implements java.lang.reflect.InvocationHandler {
     public Object obj;
     public CommandProxy(Object obj) {
         this.obj = obj;
     }
+
+    //catches all methods invoked on ServerCommandManager
     public Object invoke(Object proxy, Method m, Object[] args){
-        try {
-            System.out.println("Command Proxy successfully invoked!");
-        } catch (Exception e) {
-            throw e;
+        //TODO even though there is only one method, we should probably verify that this is the correct one anyway.
+        boolean cancelVanillaCommand = false;
+        System.out.println("Command Proxy successfully invoked!");
+
+        Object commandSender = args[0];
+        String commandString = (String) args[1];
+
+        System.out.println("Command initiated by: " + getSenderName(commandSender));
+        //pre-process command string
+        if(commandString.startsWith("/")) {
+            commandString = commandString.substring(1);
         }
-        // return something
+        String[] commandParts = commandString.split(" ");
+        System.out.println("Intercepted command: " + commandParts[0]);
+
+        if(commandParts[0].equalsIgnoreCase("test")){
+            if(isCommandSenderPlayer(commandSender)){
+                System.out.println("A player has used the test command.");
+            } else {
+                System.out.println("Silly server. Test commands are for players!");
+            }
+            cancelVanillaCommand = true;
+        }
+
+        if(!cancelVanillaCommand) {
+            try {
+                return m.invoke(obj, args); //forward to vanilla
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
-    public Object getObject(){
-        return this.obj;
+
+    public static boolean isCommandSenderPlayer(Object commandSender){
+        ArrayList<String> nonplayers = new ArrayList<String>();
+        nonplayers.add("Rcon");
+        nonplayers.add("Server");
+        nonplayers.add("@");
+        if(nonplayers.contains(getSenderName(commandSender))){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static String getSenderName(Object commandSender){
+        try {
+            Class<?> iCommandSender = Class.forName("ae");
+            return (String) iCommandSender.getDeclaredMethod("d_").invoke(commandSender);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
