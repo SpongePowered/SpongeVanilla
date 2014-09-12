daf1de6f5c2f37f07f324022518f8b6e96b3ab1e
Voltasalt <voltasalt@gmail.com>
2014-09-12 16:06:17 +0200

    Added safe casting invoke

--- src/main/java/org/granitemc/granite/reflect/ReflectionUtils.java
+++ src/main/java/org/granitemc/granite/reflect/ReflectionUtils.java
@@ -1,11 +1,25 @@
 package org.granitemc.granite.reflect;
 
-import sun.misc.Unsafe;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
 public class ReflectionUtils {
+    private static BiMap<Class<?>, Class<?>> primitives = new ImmutableBiMap.Builder<Class<?>, Class<?>>()
+            .put(byte.class, Byte.class)
+            .put(short.class, Short.class)
+            .put(int.class, Integer.class)
+            .put(long.class, Long.class)
+            .put(float.class, Float.class)
+            .put(double.class, Double.class)
+            .put(boolean.class, Boolean.class)
+            .put(char.class, Character.class)
+            .build();
+
     public static void forceStaticAccessible(Field f) {
         try {
             f.setAccessible(true);
@@ -17,5 +31,39 @@ public class ReflectionUtils {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Object invoke(Object instance, Method m, Object... args) throws InvocationTargetException {
+        try {
+            Class<?>[] paramTypes = m.getParameterTypes();
+
+            for (int i = 0; i < args.length; i++) {
+                Object obj = args[i];
+                Class<?> actual = obj.getClass();
+                Class<?> expected = paramTypes[i].getClass();
+
+                boolean canCast = false;
+                if (expected.isAssignableFrom(actual)) {
+                    canCast = true;
+                } else {
+                    // If actual is primitive and expected isn't, or vice versa (xor)
+                    if (actual.isPrimitive() ^ !expected.isPrimitive()) {
+                        if (primitives.get(actual).equals(expected)) {
+                            canCast = true;
+                        } else if (primitives.inverse().get(actual).equals(expected)) {
+                            canCast = true;
+                        }
+                    }
+                }
+
+                if (canCast) {
+                    args[i] = expected.cast(obj);
+                }
+            }
+            return m.invoke(instance, args);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
