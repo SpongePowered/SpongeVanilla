72aa06d340be87e9d4af015a70dfbb10dfbe51ca
Zeus <gamingrevamped@gmail.com>
2014-10-26 01:27:37 -0400

    Added retention policy so annotation is declared

--- GraniteAPI/src/main/java/org/granitemc/granite/api/plugin/OnDisable.java
+++ GraniteAPI/src/main/java/org/granitemc/granite/api/plugin/OnDisable.java
@@ -1,4 +1,10 @@
 package org.granitemc.granite.api.plugin;
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface OnDisable {
 }
