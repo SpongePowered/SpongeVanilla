62ad339d07dca573c1e9e81ea96deb46a21e0390
Voltasalt <voltasalt@gmail.com>
2014-11-01 10:44:52 +0100

    Add @Target annotations to the @OnEnable and @OnDisable annotations (annotationception?)

--- GraniteAPI/src/main/java/org/granitemc/granite/api/plugin/OnEnable.java
+++ GraniteAPI/src/main/java/org/granitemc/granite/api/plugin/OnEnable.java
@@ -1,8 +1,11 @@
 package org.granitemc.granite.api.plugin;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnEnable {
 }
