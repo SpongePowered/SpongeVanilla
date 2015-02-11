3fb943519486b776ae15903255a6de668c793184
AzureusNation <alphatango369@gmail.com>
2015-02-11 22:36:31 +0000

    I didn't know @Zidane loved The Little Mermaid O.o

--- src/main/java/org/granitepowered/granite/impl/entity/living/GraniteEntityAerial.java
+++ src/main/java/org/granitepowered/granite/impl/entity/living/GraniteEntityAerial.java
@@ -3,9 +3,9 @@ package org.granitepowered.granite.impl.entity.living;
 import org.granitepowered.granite.mc.MCEntityFlying;
 import org.spongepowered.api.entity.living.Aerial;
 
-public class GraniteEntityAriel<T extends MCEntityFlying> extends GraniteEntityLiving<T> implements Aerial {
+public class GraniteEntityAerial<T extends MCEntityFlying> extends GraniteEntityLiving<T> implements Aerial {
 
-    public GraniteEntityAriel(T obj) {
+    public GraniteEntityAerial(T obj) {
         super(obj);
     }
 }
--- src/main/java/org/granitepowered/granite/impl/entity/living/monster/GraniteEntityGhast.java
+++ src/main/java/org/granitepowered/granite/impl/entity/living/monster/GraniteEntityGhast.java
@@ -1,12 +1,12 @@
 package org.granitepowered.granite.impl.entity.living.monster;
 
 import com.flowpowered.math.vector.Vector3f;
-import org.granitepowered.granite.impl.entity.living.GraniteEntityAriel;
+import org.granitepowered.granite.impl.entity.living.GraniteEntityAerial;
 import org.granitepowered.granite.mc.MCEntityGhast;
 import org.spongepowered.api.entity.living.monster.Ghast;
 import org.spongepowered.api.entity.projectile.Projectile;
 
-public class GraniteEntityGhast extends GraniteEntityAriel<MCEntityGhast> implements Ghast {
+public class GraniteEntityGhast extends GraniteEntityAerial<MCEntityGhast> implements Ghast {
 
     public GraniteEntityGhast(MCEntityGhast obj) {
         super(obj);
