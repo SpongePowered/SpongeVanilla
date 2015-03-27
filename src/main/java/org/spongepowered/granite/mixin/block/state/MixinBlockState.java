9e3810a6150424a464d69525f6979129936af710
Chris Sanders <zidane@outlook.com>
2015-03-27 12:08:04 -0500

    Port over BlockState implementation.
    Fix license headers.
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/granite/mixin/block/state/MixinBlockState.java
+++ src/main/java/org/spongepowered/granite/mixin/block/state/MixinBlockState.java
@@ -24,10 +24,92 @@
  */
 package org.spongepowered.granite.mixin.block.state;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.BlockStateBase;
+import net.minecraft.block.state.IBlockState;
+import org.spongepowered.api.block.BlockProperty;
+import org.spongepowered.api.block.BlockType;
+import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(BlockState.class)
-public abstract class MixinBlockState implements org.spongepowered.api.block.BlockState {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
+@NonnullByDefault
+@Mixin(BlockState.StateImplementation.class)
+public abstract class MixinBlockState extends BlockStateBase implements org.spongepowered.api.block.BlockState {
+
+    @Override
+    public BlockType getType() {
+        return (BlockType) getBlock();
+    }
+
+    @Shadow
+    private ImmutableMap properties;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public ImmutableMap<BlockProperty<?>, ? extends Comparable<?>> getProperties() {
+        return this.properties;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<String> getPropertyNames() {
+        ImmutableMap<IProperty, Comparable<?>> properties = ((IBlockState) this).getProperties();
+        List<String> names = Lists.newArrayListWithCapacity(properties.size());
+        for (IProperty property : properties.keySet()) {
+            names.add(property.getName());
+        }
+        return Collections.unmodifiableCollection(names);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Optional<BlockProperty<?>> getPropertyByName(String name) {
+        ImmutableMap<IProperty, Comparable<?>> properties = ((IBlockState) this).getProperties();
+        for (IProperty property : properties.keySet()) {
+            if (property.getName().equals(name)) {
+                // The extra specification here is because Java auto-detects <? extends BlockProperty<?>>
+                return Optional.<BlockProperty<?>>fromNullable((BlockProperty<?>) property);
+            }
+        }
+
+        return Optional.absent();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Optional<? extends Comparable<?>> getPropertyValue(String name) {
+        ImmutableMap<IProperty, Comparable<?>> properties = ((IBlockState) this).getProperties();
+        for (IProperty property : properties.keySet()) {
+            if (property.getName().equals(name)) {
+                return Optional.fromNullable(properties.get(property));
+            }
+        }
+
+        return Optional.absent();
+    }
+
+    @Override
+    public org.spongepowered.api.block.BlockState withProperty(BlockProperty<?> property, Comparable<?> value) {
+        return (org.spongepowered.api.block.BlockState) withProperty((IProperty) property, value);
+    }
+
+    @Override
+    public org.spongepowered.api.block.BlockState cycleProperty(BlockProperty<?> property) {
+        return (org.spongepowered.api.block.BlockState) cycleProperty((IProperty) property);
+    }
+
+    @Override
+    @Deprecated
+    public byte getDataValue() {
+        return (byte) getBlock().getMetaFromState(this);
+    }
 }
