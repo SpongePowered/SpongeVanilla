4b128f0b41c51f0d9382e0caf1e938455a8e70ac
Chris Sanders <zidane@outlook.com>
2015-04-23 23:13:04 -0500

    Implement PlayerChatEvent.
    @zml2008 can you check the TODO in MixinNetHandlerPlayServer?
    @Mumfrey if this can be done better, please correct this or tell me how
    too.
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

---
+++ src/main/java/org/spongepowered/vanilla/mixin/network/MixinNetHandlerPlayServer.java
@@ -0,0 +1,116 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.vanilla.mixin.network;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.Packet;
+import net.minecraft.network.PacketThreadUtil;
+import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.C01PacketChatMessage;
+import net.minecraft.network.play.server.S02PacketChat;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatAllowedCharacters;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
+import org.apache.commons.lang3.StringUtils;
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.entity.player.PlayerChatEvent;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.common.Sponge;
+import org.spongepowered.common.text.SpongeChatComponent;
+import org.spongepowered.common.text.SpongeText;
+
+import java.util.Locale;
+
+@Mixin(NetHandlerPlayServer.class)
+public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer {
+    @Shadow private int chatSpamThresholdCount;
+    @Shadow private EntityPlayerMP playerEntity;
+    @Shadow private MinecraftServer serverController;
+    @Shadow abstract void kickPlayerFromServer(String reason);
+    @Shadow abstract void sendPacket(final Packet packetIn);
+    @Shadow abstract void handleSlashCommand(String command);
+
+    /**
+     * @author Zidane
+     *
+     * Purpose: To fire the {@link org.spongepowered.api.event.entity.player.PlayerChatEvent}.
+     * Reasoning: The overwrite is needed due to the spot where the event has to fire being awkward. Mumfrey can correct
+     * me otherwise.
+     */
+    @Overwrite
+    public void processChatMessage(C01PacketChatMessage packetIn) {
+        PacketThreadUtil.checkThreadAndEnqueue(packetIn, this, this.playerEntity.getServerForPlayer());
+
+        if (this.playerEntity.getChatVisibility() == EntityPlayer.EnumChatVisibility.HIDDEN)
+        {
+            ChatComponentTranslation chatcomponenttranslation = new ChatComponentTranslation("chat.cannotSend", new Object[0]);
+            chatcomponenttranslation.getChatStyle().setColor(EnumChatFormatting.RED);
+            this.sendPacket(new S02PacketChat(chatcomponenttranslation));
+        }
+        else
+        {
+            this.playerEntity.markPlayerActive();
+            String s = packetIn.getMessage();
+            s = StringUtils.normalizeSpace(s);
+
+            for (int i = 0; i < s.length(); ++i)
+            {
+                if (!ChatAllowedCharacters.isAllowedCharacter(s.charAt(i)))
+                {
+                    this.kickPlayerFromServer("Illegal characters in chat");
+                    return;
+                }
+            }
+
+            if (s.startsWith("/"))
+            {
+                this.handleSlashCommand(s);
+            }
+            else
+            {
+                ChatComponentTranslation chatcomponenttranslation1 = new ChatComponentTranslation("chat.type.text", new Object[] {this.playerEntity.getDisplayName(), s});
+                // Sponge Start -> Fire PlayerChatEvent
+                final PlayerChatEvent event = SpongeEventFactory.createPlayerChat(Sponge.getGame(), (Player) playerEntity, (CommandSource) playerEntity, ((SpongeChatComponent) chatcomponenttranslation1).toText());
+                Sponge.getGame().getEventManager().post(event);
+                this.serverController.getConfigurationManager().sendChatMsgImpl(((SpongeText) event.getMessage()).toComponent(Locale.ENGLISH), false); //TODO Is this locale right zml?
+                // Sponge End
+            }
+
+            this.chatSpamThresholdCount += 20;
+
+            if (this.chatSpamThresholdCount > 200 && !this.serverController.getConfigurationManager().canSendCommands(this.playerEntity.getGameProfile()))
+            {
+                this.kickPlayerFromServer("disconnect.spam");
+            }
+        }
+    }
+}
