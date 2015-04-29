eb24741584ab5404aa15b3b1f9c09a540bc5448d
Chris Sanders <zidane@outlook.com>
2015-04-29 01:00:02 -0500

    Implement PlayerInteractBlockEvent (for left click).
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/vanilla/mixin/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/vanilla/mixin/network/MixinNetHandlerPlayServer.java
@@ -27,21 +27,34 @@ package org.spongepowered.vanilla.mixin.network;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.play.INetHandlerPlayServer;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraft.network.play.server.S23PacketBlockChange;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IChatComponent;
+import net.minecraft.world.WorldServer;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.api.Server;
+import org.spongepowered.api.entity.EntityInteractionType;
+import org.spongepowered.api.entity.EntityInteractionTypes;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.entity.player.PlayerChatEvent;
 import org.spongepowered.api.event.entity.player.PlayerQuitEvent;
 import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.common.Sponge;
 import org.spongepowered.common.text.SpongeTexts;
+import org.spongepowered.common.util.VecHelper;
 
 @Mixin(NetHandlerPlayServer.class)
 public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer {
@@ -67,6 +80,21 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
         PlayerQuitEvent event = SpongeEventFactory.createPlayerQuit(Sponge.getGame(), (Player) this.playerEntity, SpongeTexts.toText(component));
         Sponge.getGame().getEventManager().post(event);
         ((Server) this.serverController).broadcastMessage(event.getQuitMessage());
+    }
+
+    @Inject(method = "processPlayerDigging", at = @At(value = "FIELD", opcode = Opcodes.GETSTATIC, target = "net/minecraft/network/play/client/C07PacketPlayerDigging$Action.START_DESTROY_BLOCK:Lnet/minecraft/network/play/client/C07PacketPlayerDigging$Action;", shift = At.Shift.AFTER, by = 2), cancellable = true)
+    public void injectProcessPlayerDigging(C07PacketPlayerDigging packetIn, CallbackInfo ci) {
+        final WorldServer worldserver = this.serverController.worldServerForDimension(this.playerEntity.dimension);
+        //TODO Quote gabizou: Calculate the clicked location on the server. Hmmm, sounds fun...come back and do later -_-.
+        boolean cancelled = Sponge.getGame().getEventManager().post(SpongeEventFactory.createPlayerInteractBlock(Sponge.getGame(), new Cause(null, playerEntity, null), (Player) playerEntity, new Location((World) worldserver, VecHelper.toVector(packetIn.getPosition())), EntityInteractionTypes.ATTACK, null));
+        if (cancelled) {
+            ci.cancel();
+            this.playerEntity.playerNetServerHandler.sendPacket(new S23PacketBlockChange(worldserver, packetIn.getPosition()));
+            TileEntity tileentity = worldserver.getTileEntity(packetIn.getPosition());
+            if (tileentity != null) {
+                this.playerEntity.playerNetServerHandler.sendPacket(tileentity.getDescriptionPacket());
+            }
+        }
     }
 
 }
