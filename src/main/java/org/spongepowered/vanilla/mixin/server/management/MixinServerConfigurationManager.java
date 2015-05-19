4f1e9f5392a7841b77d5cceb5c0dc58466299b8f
Chris Sanders <zidane@outlook.com>
2015-05-18 23:56:07 -0500

    Correctly fire onPlayerJoin.
    Unfortunately, this requires an overwrite. On the bright side, this
    actually fires PlayerJoin in the correct spot as the API contract demands
    which allows the location and message to be set before updating the
    player's client or the server itself.
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/vanilla/mixin/server/management/MixinServerConfigurationManager.java
+++ src/main/java/org/spongepowered/vanilla/mixin/server/management/MixinServerConfigurationManager.java
@@ -24,52 +24,169 @@
  */
 package org.spongepowered.vanilla.mixin.server.management;
 
+import com.mojang.authlib.GameProfile;
+import io.netty.buffer.Unpooled;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.NetworkManager;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.play.server.S01PacketJoinGame;
+import net.minecraft.network.play.server.S05PacketSpawnPosition;
+import net.minecraft.network.play.server.S09PacketHeldItemChange;
+import net.minecraft.network.play.server.S1DPacketEntityEffect;
+import net.minecraft.network.play.server.S39PacketPlayerAbilities;
+import net.minecraft.network.play.server.S3FPacketCustomPayload;
+import net.minecraft.network.play.server.S41PacketServerDifficulty;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerProfileCache;
 import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
-import org.spongepowered.api.Server;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.storage.WorldInfo;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.event.SpongeEventFactory;
 import org.spongepowered.api.event.entity.player.PlayerJoinEvent;
 import org.spongepowered.api.event.entity.player.PlayerRespawnEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.common.Sponge;
 import org.spongepowered.common.text.SpongeTexts;
 
-import javax.annotation.Nullable;
+import java.util.Iterator;
 
 @Mixin(ServerConfigurationManager.class)
 public abstract class MixinServerConfigurationManager {
 
     @Shadow private MinecraftServer mcServer;
-    @Nullable private IChatComponent joinMessage;
+    @Shadow private static Logger logger;
+    @Shadow abstract NBTTagCompound readPlayerDataFromFile(EntityPlayerMP playerIn);
+    @Shadow abstract void setPlayerGameTypeBasedOnOther(EntityPlayerMP playerIn1, EntityPlayerMP playerIn2, World world);
+    @Shadow abstract MinecraftServer getServerInstance();
+    @Shadow abstract void func_96456_a(ServerScoreboard scoreboardIn, EntityPlayerMP playerIn);
+    @Shadow abstract int getMaxPlayers();
+    @Shadow abstract void sendChatMsg(IChatComponent component);
+    @Shadow abstract void playerLoggedIn(EntityPlayerMP playerIn);
+    @Shadow abstract void updateTimeAndWeatherForPlayer(EntityPlayerMP playerIn, WorldServer worldIn);
 
-    @Redirect(method = "initializeConnectionToPlayer", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/server/management/ServerConfigurationManager;sendChatMsg(Lnet/minecraft/util/IChatComponent;)V"))
-    public void initializeConnectionToPlayerRedirectSendChatMsg(ServerConfigurationManager this$0, IChatComponent component) {
-        this.joinMessage = component;
-    }
+    /**
+     * @Author Zidane
+     *
+     * Purpose: To fire the {@link PlayerJoinEvent} before instructing the player's client where the join will occur at.
+     * Reasoning: We want to be able to control the location and the message. I moved up the logic for constructing the message
+     * before the sending of the chat message regarding where they are spawning at. This also has the added benefit of letting a plugin cleanly
+     * set the location before all logic applies (in sending the packets to prepare a client for spawn)
+     * @param netManager Injected network manager param
+     * @param playerIn Injected player param
+     */
+    @Overwrite
+    public void initializeConnectionToPlayer(NetworkManager netManager, EntityPlayerMP playerIn) {
+        GameProfile gameprofile = playerIn.getGameProfile();
+        PlayerProfileCache playerprofilecache = this.mcServer.getPlayerProfileCache();
+        GameProfile gameprofile1 = playerprofilecache.getProfileByUUID(gameprofile.getId());
+        String s = gameprofile1 == null ? gameprofile.getName() : gameprofile1.getName();
+        playerprofilecache.addEntry(gameprofile);
+        NBTTagCompound nbttagcompound = this.readPlayerDataFromFile(playerIn);
+        playerIn.setWorld(this.mcServer.worldServerForDimension(playerIn.dimension));
+        playerIn.theItemInWorldManager.setWorld((WorldServer)playerIn.worldObj);
+        String s1 = "local";
 
-    @Inject(method = "initializeConnectionToPlayer", at = @At("RETURN"))
-    public void initializeConnectionToPlayerInject(NetworkManager netManager, EntityPlayerMP player, CallbackInfo ci) {
-        final PlayerJoinEvent event = SpongeEventFactory.createPlayerJoin(Sponge.getGame(), (Player) player, ((Player) player).getLocation(),
-                SpongeTexts.toText(this.joinMessage));
-        this.joinMessage = null;
+        if (netManager.getRemoteAddress() != null)
+        {
+            s1 = netManager.getRemoteAddress().toString();
+        }
+
+        // Sponge Start
+
+        // Move logic for creating join message up here
+        ChatComponentTranslation chatcomponenttranslation;
+
+        if (!playerIn.getCommandSenderName().equalsIgnoreCase(s))
+        {
+            chatcomponenttranslation = new ChatComponentTranslation("multiplayer.player.joined.renamed", new Object[] {playerIn.getDisplayName(), s});
+        }
+        else
+        {
+            chatcomponenttranslation = new ChatComponentTranslation("multiplayer.player.joined", new Object[] {playerIn.getDisplayName()});
+        }
+
+        chatcomponenttranslation.getChatStyle().setColor(EnumChatFormatting.YELLOW);
+
+        // Create the handler here (so the player's gets set)
+        NetHandlerPlayServer nethandlerplayserver = new NetHandlerPlayServer(this.mcServer, netManager, playerIn);
+
+        // Fire PlayerJoinEvent
+        final PlayerJoinEvent event = SpongeEventFactory.createPlayerJoin(Sponge.getGame(), (Player) playerIn, ((Player) playerIn).getLocation(),
+                SpongeTexts.toText(chatcomponenttranslation));
+        System.out.println(event);
         Sponge.getGame().getEventManager().post(event);
+        // Set the resolved location of the event onto the player
+        ((Player) playerIn).setLocation(event.getLocation());
 
-        // TODO See if this is fine for world changes
-        ((Player) player).setLocation(event.getLocation());
+        // Sponge End
 
-        // Send (possibly changed) join message
-        ((Server) this.mcServer).broadcastMessage(event.getNewMessage());
+        logger.info(playerIn.getCommandSenderName() + "[" + s1 + "] logged in with entity id " + playerIn.getEntityId() + " at (" + playerIn
+                .posX + ", " + playerIn.posY + ", " + playerIn.posZ + ")");
+        WorldServer worldserver = this.mcServer.worldServerForDimension(playerIn.dimension);
+        WorldInfo worldinfo = worldserver.getWorldInfo();
+        BlockPos blockpos = worldserver.getSpawnPoint();
+        this.setPlayerGameTypeBasedOnOther(playerIn, (EntityPlayerMP)null, worldserver);
+        nethandlerplayserver.sendPacket(new S01PacketJoinGame(playerIn.getEntityId(), playerIn.theItemInWorldManager.getGameType(), worldinfo.isHardcoreModeEnabled(), worldserver.provider.getDimensionId(), worldserver.getDifficulty(), this.getMaxPlayers(), worldinfo.getTerrainType(), worldserver.getGameRules().getGameRuleBooleanValue("reducedDebugInfo")));
+        nethandlerplayserver.sendPacket(new S3FPacketCustomPayload("MC|Brand", (new PacketBuffer(Unpooled.buffer())).writeString(this.getServerInstance().getServerModName())));
+        nethandlerplayserver.sendPacket(new S41PacketServerDifficulty(worldinfo.getDifficulty(), worldinfo.isDifficultyLocked()));
+        nethandlerplayserver.sendPacket(new S05PacketSpawnPosition(blockpos));
+        nethandlerplayserver.sendPacket(new S39PacketPlayerAbilities(playerIn.capabilities));
+        nethandlerplayserver.sendPacket(new S09PacketHeldItemChange(playerIn.inventory.currentItem));
+        playerIn.getStatFile().func_150877_d();
+        playerIn.getStatFile().func_150884_b(playerIn);
+        this.func_96456_a((ServerScoreboard)worldserver.getScoreboard(), playerIn);
+        this.mcServer.refreshStatusNextTick();
+
+        this.sendChatMsg(SpongeTexts.toComponent(event.getNewMessage()));
+        this.playerLoggedIn(playerIn);
+        nethandlerplayserver.setPlayerLocation(playerIn.posX, playerIn.posY, playerIn.posZ, playerIn.rotationYaw, playerIn.rotationPitch);
+        this.updateTimeAndWeatherForPlayer(playerIn, worldserver);
+
+        if (this.mcServer.getResourcePackUrl().length() > 0)
+        {
+            playerIn.loadResourcePack(this.mcServer.getResourcePackUrl(), this.mcServer.getResourcePackHash());
+        }
+
+        Iterator iterator = playerIn.getActivePotionEffects().iterator();
+
+        while (iterator.hasNext())
+        {
+            PotionEffect potioneffect = (PotionEffect)iterator.next();
+            nethandlerplayserver.sendPacket(new S1DPacketEntityEffect(playerIn.getEntityId(), potioneffect));
+        }
+
+        playerIn.addSelfToInternalCraftingInventory();
+
+        if (nbttagcompound != null && nbttagcompound.hasKey("Riding", 10))
+        {
+            Entity entity = EntityList.createEntityFromNBT(nbttagcompound.getCompoundTag("Riding"), worldserver);
+
+            if (entity != null)
+            {
+                entity.forceSpawn = true;
+                worldserver.spawnEntityInWorld(entity);
+                playerIn.mountEntity(entity);
+                entity.forceSpawn = false;
+            }
+        }
     }
 
     /**
