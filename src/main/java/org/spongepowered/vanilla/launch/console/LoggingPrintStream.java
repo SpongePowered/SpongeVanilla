d375cb81d5e723e59e9b9b90ee670275c126365c
Minecrell <dev@minecrell.net>
2015-05-22 19:47:31 +0200

    Fix log wrapper handling printf incorrectly, fixes @Mumfrey Also added option to disable the redirecting

--- src/main/java/org/spongepowered/vanilla/launch/console/LoggingPrintStream.java
+++ src/main/java/org/spongepowered/vanilla/launch/console/LoggingPrintStream.java
@@ -24,46 +24,29 @@
  */
 package org.spongepowered.vanilla.launch.console;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.PrintStream;
 
-public class LoggingOutputStream extends ByteArrayOutputStream {
+public class LoggingPrintStream extends PrintStream {
 
-    private static final String SEPARATOR = System.getProperty("line.separator");
-    private final Logger logger;
-    private final Level level;
-    boolean flush = true;
+    private final LoggingOutputStream out;
 
-    public LoggingOutputStream(Logger logger, Level level) {
-        this.logger = checkNotNull(logger, "logger");
-        this.level = checkNotNull(level, "level");
+    public LoggingPrintStream(Logger logger, Level level) {
+        this(new LoggingOutputStream(logger, level));
+    }
+
+    public LoggingPrintStream(LoggingOutputStream out) {
+        super(out, true);
+        this.out = out;
     }
 
     @Override
-    public void flush() throws IOException {
-        if (!this.flush) {
-            return;
-        }
-
-        String message = toString();
-        reset();
-
-        if (!message.isEmpty() && !message.equals(SEPARATOR)) {
-            if (message.endsWith(SEPARATOR)) {
-                message = message.substring(0, message.length() - SEPARATOR.length());
-            }
-
-            if (message.charAt(message.length() - 1) == '\n') {
-                message = message.substring(0, message.length() - 1);
-            }
-
-            this.logger.log(this.level, message);
-        }
+    public void write(byte[] buf, int off, int len) {
+        this.out.flush = false;
+        super.write(buf, off, len);
+        this.out.flush = true;
     }
 
 }
