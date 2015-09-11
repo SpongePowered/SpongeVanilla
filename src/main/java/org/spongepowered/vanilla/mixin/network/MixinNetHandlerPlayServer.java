71d8d29a84578a2fde882d8eaedd8767b5ed2aaf
Chris Sanders <zidane@outlook.com>
2015-09-10 22:05:13 -0500

    Fire UseItemStackEvent/InteractBlockEvent.Use for right-click air.
    This fires InteractBlock.Use for when you right click air with empty
    handed or with an ItemStack. If the latter, we fire
    UseItemStackEvent.Start first, see if its cancelled, then perform the item
    usage.
    
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/spongepowered/vanilla/mixin/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/vanilla/mixin/network/MixinNetHandlerPlayServer.java
@@ -27,6 +27,7 @@ package org.spongepowered.vanilla.mixin.network;
 import com.flowpowered.math.vector.Vector3d;
 import com.google.common.base.Optional;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.NetHandlerPlayServer;
 import net.minecraft.network.play.INetHandlerPlayServer;
 import net.minecraft.network.play.client.C07PacketPlayerDigging;
@@ -34,9 +35,12 @@ import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.network.play.server.S23PacketBlockChange;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IChatComponent;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.WorldServer;
+import org.spongepowered.api.block.BlockSnapshot;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.SpongeEventFactory;
 import org.spongepowered.api.event.block.InteractBlockEvent;
@@ -44,10 +48,11 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.command.MessageSinkEvent;
 import org.spongepowered.api.event.inventory.UseItemStackEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
-import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackTransaction;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.sink.MessageSink;
+import org.spongepowered.api.util.blockray.BlockRay;
+import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -56,18 +61,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.common.Sponge;
 import org.spongepowered.common.registry.SpongeGameRegistry;
 import org.spongepowered.common.text.SpongeTexts;
 import org.spongepowered.common.util.VecHelper;
-import org.spongepowered.vanilla.util.EntityUtils;
 
 @Mixin(NetHandlerPlayServer.class)
 public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer {
 
     @Shadow private EntityPlayerMP playerEntity;
     @Shadow private MinecraftServer serverController;
-    boolean isRightClickAirCancelled;
 
     @Redirect(method = "processChatMessage",
             at = @At(value = "INVOKE",
@@ -102,9 +106,10 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
         ci.cancel();
         final WorldServer worldserver = this.serverController.worldServerForDimension(this.playerEntity.dimension);
         final Location<World> location = new Location<World>((World) worldserver, VecHelper.toVector(packetIn.getPosition()));
-        //TODO Quote gabizou: Calculate the clicked location on the server. Hmmm, sounds fun...come back and do later -_-.
+        final Optional<BlockRayHit<World>> optBlockHit = BlockRay.from((Entity) playerEntity).end();
+        final Optional<Vector3d> optInteractionPoint = Optional.of(optBlockHit.get().getNormal());
         final InteractBlockEvent.Attack event = SpongeEventFactory.createInteractBlockEventAttack(Sponge.getGame(), Cause.of(playerEntity),
-                Optional.<Vector3d>absent(), location.createSnapshot(), SpongeGameRegistry.directionMap.inverse().get(packetIn.getFacing()));
+                optInteractionPoint, location.createSnapshot(), SpongeGameRegistry.directionMap.inverse().get(packetIn.getFacing()));
         boolean revert = Sponge.getGame().getEventManager().post(event);
 
         if (!revert) {
@@ -122,53 +127,60 @@ public abstract class MixinNetHandlerPlayServer implements INetHandlerPlayServer
     }
 
     /**
-     * Invoke before {@code packetIn.getPlacedBlockDirection() == 255} (line 576 in source) to reset "isRightClickAirCancelled" flag.
-     * @param packetIn Injected packet param
-     * @param ci Info to provide mixin on how to handle the callback
-     */
-    @Inject(method = "processPlayerBlockPlacement",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/network/play/client/C08PacketPlayerBlockPlacement;getPlacedBlockDirection()I",
-                    ordinal = 1))
-    public void injectBeforeBlockDirectionCheck(C08PacketPlayerBlockPlacement packetIn, CallbackInfo ci) {
-        this.isRightClickAirCancelled = false;
-    }
-
-    /**
      * Invoke after {@code packetIn.getPlacedBlockDirection() == 255} (line 576 in source) to fire {@link org.spongepowered.api.event.entity
      * .player.PlayerInteractEvent} on right click with air with item in-hand.
      * @param packetIn Injected packet param
      * @param ci Info to provide mixin on how to handle the callback
      */
-    @Inject(method = "processPlayerBlockPlacement",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/network/play/client/C08PacketPlayerBlockPlacement;getPlacedBlockDirection()I",
-                    ordinal = 1, shift = At.Shift.BY, by = 3))
-    public void injectBeforeItemStackCheck(C08PacketPlayerBlockPlacement packetIn, CallbackInfo ci) {
-        final MovingObjectPosition objectPosition = EntityUtils.rayTraceFromEntity(this.playerEntity, 4, 1);
-        if (objectPosition != null && objectPosition.entityHit == null) {
-            // TODO Work with blood and figure out entire chain
-            final ItemStackTransaction transaction = new ItemStackTransaction(((ItemStack) playerEntity.getItemInUse()).createSnapshot());
-            final UseItemStackEvent event = SpongeEventFactory.createUseItemStackEventStart(Sponge.getGame(), Cause.of(playerEntity), transaction);
-            this.isRightClickAirCancelled = Sponge.getGame().getEventManager().post(event);
-        }
+    @Inject(method = "processPlayerBlockPlacement", at = @At(value = "RETURN", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void injectBeforeItemStackCheck(C08PacketPlayerBlockPlacement packetIn, CallbackInfo ci, WorldServer worldserver, ItemStack itemstack,
+            boolean flag, BlockPos blockpos, EnumFacing enumfacing) {
+        final Optional<Vector3d> optInteractionPoint = Optional.of(new Vector3d(packetIn.getPlacedBlockOffsetX(), packetIn
+                .getPlacedBlockOffsetY(), packetIn.getPlacedBlockOffsetZ()));
+        final BlockSnapshot current = ((World) playerEntity.worldObj).createSnapshot(blockpos.getX(), blockpos.getY(), blockpos.getZ());
+        final InteractBlockEvent.Use event =
+                SpongeEventFactory.createInteractBlockEventUse(Sponge.getGame(), Cause.of(playerEntity), optInteractionPoint, current,
+                        SpongeGameRegistry.directionMap.inverse().get(enumfacing.getOpposite()));
+        Sponge.getGame().getEventManager().post(event);
     }
 
     /**
-     * Invoke before {@code itemstack == null} (line 578 in source) to stop the logic for using an item if {@link org.spongepowered.api.event
-     * .entity.player.PlayerInteractEvent} was cancelled.
+     * Invoke after {@code itemstack == null} (line 578 in source) to stop the logic for using an item if {@link UseItemStackEvent} was cancelled.
      * @param packetIn Injected packet param
      * @param ci Info to provide mixin on how to handle the callback
      */
     @Inject(method = "processPlayerBlockPlacement", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/server/management/ItemInWorldManager;tryUseItem(Lnet/minecraft/entity/player/EntityPlayer;"
                     + "Lnet/minecraft/world/World;Lnet/minecraft/item/ItemStack;)Z",
-            ordinal = 0), cancellable = true)
-    public void injectAfterItemStackCheck(C08PacketPlayerBlockPlacement packetIn, CallbackInfo ci) {
+            ordinal = 0), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    public void injectAfterItemStackCheck(C08PacketPlayerBlockPlacement packetIn, CallbackInfo ci, WorldServer worldserver, ItemStack itemstack,
+            boolean flag, BlockPos blockpos, EnumFacing enumfacing) {
         // Handle interact logic
         ci.cancel();
-        if (!this.isRightClickAirCancelled) {
+
+        final ItemStackTransaction itemStackTransaction = new ItemStackTransaction(((org.spongepowered.api.item.inventory.ItemStack)
+                itemstack).createSnapshot());
+        final UseItemStackEvent.Start event = SpongeEventFactory.createUseItemStackEventStart(Sponge.getGame(), Cause.of(playerEntity),
+                itemStackTransaction);
+        boolean cancelled = Sponge.getGame().getEventManager().post(event);
+
+        if (!cancelled) {
+            // Play out item use
             this.playerEntity.theItemInWorldManager
                     .tryUseItem(this.playerEntity, this.serverController.worldServerForDimension(this.playerEntity.dimension),
                             this.playerEntity.inventory.getCurrentItem());
+
+            // Fire interact block on air block we swung our itemstack at
+            final Optional<Vector3d> optInteractionPoint = Optional.of(new Vector3d(packetIn.getPlacedBlockOffsetX(), packetIn
+                    .getPlacedBlockOffsetY(), packetIn.getPlacedBlockOffsetZ()));
+
+            final Optional<BlockRayHit<World>> optBlockHit = BlockRay.from((Entity) playerEntity).filter(BlockRay.<World>onlyAirFilter()).end();
+            final BlockSnapshot current = ((World) worldserver).createSnapshot(optBlockHit.get().getBlockPosition());
+            final InteractBlockEvent.Use otherEvent =
+                    SpongeEventFactory.createInteractBlockEventUse(Sponge.getGame(), Cause.of(playerEntity), optInteractionPoint, current,
+                            SpongeGameRegistry.directionMap.inverse().get(enumfacing.getOpposite()));
+            Sponge.getGame().getEventManager().post(otherEvent);
+
         }
     }
 }
