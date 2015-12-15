cd7a298a1f2315cfa46a3afc232a4bcf97c62cb1
Minecrell <dev@minecrell.net>
2015-12-15 18:39:57 +0100

    Implement DestructEntityEvent.Death

---
+++ src/main/java/org/spongepowered/server/mixin/entity/living/MixinEntityLivingDeath.java
@@ -0,0 +1,103 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.server.mixin.entity.living;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSource;
+import net.minecraft.world.World;
+import org.spongepowered.api.entity.living.Living;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.event.SpongeEventFactory;
+import org.spongepowered.api.event.cause.Cause;
+import org.spongepowered.api.event.cause.NamedCause;
+import org.spongepowered.api.event.entity.DestructEntityEvent;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.sink.MessageSink;
+import org.spongepowered.api.text.sink.MessageSinks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.common.SpongeImpl;
+import org.spongepowered.common.data.util.NbtDataUtil;
+import org.spongepowered.common.interfaces.entity.IMixinEntity;
+import org.spongepowered.common.text.SpongeTexts;
+import org.spongepowered.common.util.StaticMixinHelper;
+
+import java.util.Optional;
+
+@Mixin({EntityLivingBase.class, EntityPlayer.class, EntityPlayerMP.class})
+public abstract class MixinEntityLivingDeath extends EntityLivingBase {
+
+    public MixinEntityLivingDeath(World worldIn) {
+        super(worldIn);
+    }
+
+    @Inject(method = "onDeath", at = @At("HEAD"), cancellable = true)
+    private void callDestructEntityEventDeath(DamageSource source, CallbackInfo ci) {
+        MessageSink messageSink = this instanceof Player ? ((Player) this).getMessageSink() : MessageSinks.toNone();
+        Text deathMessage = SpongeTexts.toText(getCombatTracker().getDeathMessage());
+
+        Optional<User> sourceCreator = Optional.empty();
+
+        Cause cause;
+        if (source instanceof EntityDamageSource) {
+            EntityDamageSource damageSource = (EntityDamageSource) source;
+            IMixinEntity spongeEntity = (IMixinEntity) damageSource.getSourceOfDamage();
+            sourceCreator = spongeEntity.getTrackedPlayer(NbtDataUtil.SPONGE_ENTITY_CREATOR);
+        }
+
+        if (sourceCreator.isPresent()) {
+            cause = Cause.of(NamedCause.source(source), NamedCause.of("Victim", this), NamedCause.owner(sourceCreator.get()));
+        } else {
+            cause = Cause.of(NamedCause.source(source), NamedCause.of("Victim", this));
+        }
+
+        DestructEntityEvent.Death event = SpongeEventFactory.createDestructEntityEventDeath(SpongeImpl.getGame(), cause, deathMessage,
+                deathMessage, messageSink, messageSink, (Living) this);
+        if (!SpongeImpl.postEvent(event)) {
+            // TODO: Fix empty messages properly
+            deathMessage = event.getMessage();
+            if (deathMessage != Texts.of()) {
+                event.getSink().sendMessage(deathMessage);
+            }
+
+            // Store cause for drop event which is called after this event
+            if (sourceCreator.isPresent()) {
+                StaticMixinHelper.dropCause = Cause.of(NamedCause.source(this), NamedCause.of("Attacker", source), NamedCause.owner(sourceCreator.get()));
+            } else {
+                StaticMixinHelper.dropCause = Cause.of(NamedCause.source(this), NamedCause.of("Attacker", source));
+            }
+        } else {
+            ci.cancel(); // Cancel entity death
+        }
+    }
+
+}
