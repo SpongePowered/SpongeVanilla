6742c5d18d4c7c508fa137b55c309f902d5526fc
Minecrell <dev@minecrell.net>
2016-04-29 17:46:15 +0200

    Fix compile warnings

--- src/main/java/org/spongepowered/server/mixin/world/MixinWorldServer.java
+++ src/main/java/org/spongepowered/server/mixin/world/MixinWorldServer.java
@@ -76,7 +76,7 @@ public abstract class MixinWorldServer extends net.minecraft.world.World {
     // This causes "phantom rain" on the client, sunny and rainy weather at the same time
     @Redirect(method = "updateWeather", require = 4, at = @At(value = "INVOKE",
             target = "Lnet/minecraft/server/management/ServerConfigurationManager;sendPacketToAllPlayers(Lnet/minecraft/network/Packet;)V"))
-    private void onSendWeatherPacket(ServerConfigurationManager manager, Packet packet) {
+    private void onSendWeatherPacket(ServerConfigurationManager manager, Packet<?> packet) {
         manager.sendPacketToAllPlayersInDimension(packet, this.provider.getDimensionId());
     }
 
