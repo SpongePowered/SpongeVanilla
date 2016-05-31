418b347657e483cd41bd9d1346d66844b6eb3f69
Chris Sanders <zidane@spongepowered.org>
2016-05-30 23:18:52 -0500

    Force Vanilla methods to call derivatives with Teleporter.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/server/mixin/server/MixinServerConfigurationManager.java
+++ src/main/java/org/spongepowered/server/mixin/server/MixinServerConfigurationManager.java
@@ -2,13 +2,19 @@ package org.spongepowered.server.mixin.server;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = ServerConfigurationManager.class, priority = 999)
 public abstract class MixinServerConfigurationManager {
+
+    @Shadow @Final MinecraftServer mcServer;
 
     // Note: This is needed to add Forge NMS-only method so common can overwrite
     public void transferPlayerToDimension(EntityPlayerMP playerIn, int dimension, Teleporter teleporter) {
@@ -17,5 +23,15 @@ public abstract class MixinServerConfigurationManager {
 
     public void transferEntityToWorld(Entity entityIn, int fromDimensionId, WorldServer fromWorld, WorldServer toWorld, net.minecraft.world.Teleporter teleporter) {
 
+    }
+
+    @Overwrite
+    public void transferPlayerToDimension(EntityPlayerMP playerMP, int dimension) {
+        this.transferPlayerToDimension(playerMP, dimension, mcServer.worldServerForDimension(dimension).getDefaultTeleporter());
+    }
+
+    @Overwrite
+    public void transferEntityToWorld(Entity entityIn, int fromDimensionId, WorldServer fromWorld, WorldServer toWorld) {
+        this.transferEntityToWorld(entityIn, fromDimensionId, fromWorld, toWorld, toWorld.getDefaultTeleporter());
     }
 }
