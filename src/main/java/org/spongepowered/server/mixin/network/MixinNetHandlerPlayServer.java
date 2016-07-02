b5a5139921d714237ca299e486abf9cc359b995e
Aaron Hill <aa1ronham@gmail.com>
2016-07-02 10:06:53 -0400

    Update SpongeCommon

--- src/main/java/org/spongepowered/server/mixin/network/MixinNetHandlerPlayServer.java
+++ src/main/java/org/spongepowered/server/mixin/network/MixinNetHandlerPlayServer.java
@@ -119,18 +119,21 @@ public abstract class MixinNetHandlerPlayServer implements RemoteConnection, IMi
         }
 
         ci.cancel();
+
+        Cause cause = Cause.builder().named(NamedCause.SOURCE, this.playerEntity).named(NamedCause.of("NetHandler", this)).build();
+
         if (name.equals(REGISTER_CHANNEL)) {
             final String channels = packet.getBufferData().toString(StandardCharsets.UTF_8);
             for (String channel : CHANNEL_SPLITTER.split(channels)) {
                 if (this.registeredChannels.add(channel)) {
-                    SpongeImpl.postEvent(SpongeEventFactory.createChannelRegistrationEventRegister(Cause.of(NamedCause.source(this)), channel, Optional.of((Player) this.playerEntity)));
+                    SpongeImpl.postEvent(SpongeEventFactory.createChannelRegistrationEventRegister(cause, channel);
                 }
             }
         } else if (name.equals(UNREGISTER_CHANNEL)) {
             final String channels = packet.getBufferData().toString(StandardCharsets.UTF_8);
             for (String channel : CHANNEL_SPLITTER.split(channels)) {
                 if (this.registeredChannels.remove(channel)) {
-                    SpongeImpl.postEvent(SpongeEventFactory.createChannelRegistrationEventUnregister(Cause.of(NamedCause.source(this)), channel, Optional.of((Player) this.playerEntity)));
+                    SpongeImpl.postEvent(SpongeEventFactory.createChannelRegistrationEventUnregister(cause, channel));
                 }
             }
         } else {
