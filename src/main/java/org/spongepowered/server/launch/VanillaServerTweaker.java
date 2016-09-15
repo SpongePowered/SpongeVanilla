cca6f8d56cb99d3debfb41a7ca6a0250a571c2ec
Minecrell <dev@minecrell.net>
2016-09-15 13:46:28 +0200

    Add classloader exclusion for PluginContainer. Fixes #268
    - Update SpongeCommon

--- src/main/java/org/spongepowered/server/launch/VanillaServerTweaker.java
+++ src/main/java/org/spongepowered/server/launch/VanillaServerTweaker.java
@@ -80,6 +80,8 @@ public final class VanillaServerTweaker implements ITweaker {
         loader.addClassLoaderExclusion("org.spongepowered.common.launch.");
         loader.addClassLoaderExclusion("org.spongepowered.server.launch.");
         loader.addClassLoaderExclusion("org.spongepowered.plugin.");
+        // TODO: Move code referencing PluginContainer out of launch package
+        loader.addClassLoaderExclusion("org.spongepowered.api.plugin.");
 
         // The server GUI won't work if we don't exclude this: log4j2 wants to have this in the same classloader
         loader.addClassLoaderExclusion("com.mojang.util.QueueLogAppender");
