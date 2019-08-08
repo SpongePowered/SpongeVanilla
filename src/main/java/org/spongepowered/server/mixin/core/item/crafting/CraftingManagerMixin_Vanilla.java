98543f040885f2866cb3741bfa56c0ce28658270
Parker Lougheed <parkermeronat@gmail.com>
2019-08-08 03:29:48 -0500

    Allow overwriting vanilla recipes - Fixes #396

---
+++ src/main/java/org/spongepowered/server/mixin/core/item/crafting/CraftingManagerMixin_Vanilla.java
@@ -0,0 +1,21 @@
+package org.spongepowered.server.mixin.core.item.crafting;
+
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CraftingManager.class)
+public abstract class CraftingManagerMixin_Vanilla {
+
+    @Inject(method = "register(Lnet/minecraft/util/ResourceLocation;Lnet/minecraft/item/crafting/IRecipe;)V", at = @At(value = "NEW"), cancellable = true)
+    private static void register(final ResourceLocation name, final IRecipe recipe, final CallbackInfo ci) {
+        final int id = CraftingManager.REGISTRY.getIDForObject(CraftingManager.REGISTRY.getObject(name));
+        CraftingManager.REGISTRY.register(id, name, recipe);
+        ci.cancel();
+    }
+
+}
