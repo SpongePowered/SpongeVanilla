80572d89319d3a1a1734a74a6119fd32ec5d1e56
James <jameshendry05@gmail.com>
2020-09-01 00:16:25 +0100

    Renamed MapDataBridgeVanilla

--- src/main/java/org/spongepowered/server/mixin/core/map/MapDataBridgeMixin_Vanilla.java
+++ src/main/java/org/spongepowered/server/mixin/core/map/MapDataBridgeMixin_Vanilla.java
@@ -30,7 +30,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.common.bridge.world.storage.MapDataBridge;
 
 @Mixin(MapData.class)
-public abstract class MapDataBridgeMixinVanilla implements MapDataBridge {
+public abstract class MapDataBridgeMixin_Vanilla implements MapDataBridge {
     @Shadow
     public byte dimension;
 
